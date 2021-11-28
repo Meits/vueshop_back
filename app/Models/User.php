bf3f4f5d6a51d84f35f67b8e413b134614b8a153
Viktor <meitslin@gmail.com>
2021-11-28 20:55:15 +0200

    Add register

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
+        'phone',
         'email',
         'password',
     ];
